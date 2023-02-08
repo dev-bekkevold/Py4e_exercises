1c9350e3c6c870a85369a178352d75a0c699c4cb
dev-bekkevold <dev.bekkevold@gmail.com>
2023-02-08 20:52:03 +0100

    new exersices

--- ex_3.1.py
+++ ex_3.1.py
@@ -1,6 +1,9 @@
 
-h = float(input('How many hours have you worked?'))
-r = float(input('What is the rate?'))
+hrs = input('How many hours have you worked?')
+rate = input('What is the rate?')
+
+h = float(hrs)
+r = float(rate)
 
 normalpay = (h * r)
 overpay = ((h - 40) * 1.5 * r) + (40 * 10.5)
